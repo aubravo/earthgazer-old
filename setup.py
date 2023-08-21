3829ef0d7c71304233e83e3ae9039c784234a39a
Alvaro Bravo <alvaroubravo@gmail.com>
2023-08-20 23:17:01 -0600

    fix readthedocs failing build

--- setup.py
+++ setup.py
@@ -40,10 +40,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-        # uncomment if you test on these interpreters:
-        # "Programming Language :: Python :: Implementation :: IronPython",
-        # "Programming Language :: Python :: Implementation :: Jython",
-        # "Programming Language :: Python :: Implementation :: Stackless",
         "Topic :: Utilities",
     ],
     project_urls={
@@ -71,8 +67,6 @@ setup(
         "fsspec",
         "gcsfs",
         "click_aliases"
-        # "dask",
-        # "satpy",
     ],
     extras_require={
         # eg:
